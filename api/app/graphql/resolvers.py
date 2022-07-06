1efebd6b4d4896bec7e026b5286e46dadea42a44
Filipe Lopes <filiperocklopes@gmail.com>
2022-07-06 00:30:07 -0300

    add more fixtures

--- api/app/graphql/resolvers.py
+++ api/app/graphql/resolvers.py
@@ -5,7 +5,7 @@ import random
 
 from app.serializers import ReceitaSchema
 from app.models.IniciarConsulta import Ciap, Cid10, Procedimento
-from app.models.Medicamento import Medicamento, MedicamentoPrincipioAtivo, Receita, ViaAdministracao
+from app.models.Medicamento import FormaFarmaceutica, Medicamento, MedicamentoPrincipioAtivo, Receita, TipoFrequencia, TipoReceita, ViaAdministracao
 
 random_instance = random.Random(500)
 
@@ -31,7 +31,16 @@ def fixtures(*_, table=None, model=None):
         return [{'model': model, 'pk': exame.co_proced, 'fields': json.dumps({'code': exame.co_proced, 'name': exame.no_proced, 'note': None })} for exame in exame_list_all]
     elif table == 'medicamento':
         medicamento_list_all = MedicamentoPrincipioAtivo.query.all()
-        return [{'model': model, 'pk': m.medicamento.co_seq_medicamento, 'fields': json.dumps({'active_principle': m.medicamento.no_principio_ativo, 'concentration': m.medicamento.ds_concentracao, 'pharmaceutical_form': m.medicamento.co_forma_farmaceutica, 'supply_unit': m.medicamento.ds_unidade_fornecimento, 'recipe_type': m.principio_ativo.lista_medicamento.tipo_receita.no_tipo_receita, 'route_id': None, })} for m in medicamento_list_all]
+        return [{'model': model, 'pk': m.medicamento.co_seq_medicamento, 'fields': json.dumps({'active_principle': m.medicamento.no_principio_ativo, 'concentration': m.medicamento.ds_concentracao, 'pharmaceutical_form_id': m.medicamento.co_forma_farmaceutica, 'supply_unit_id': m.medicamento.ds_unidade_fornecimento, 'recipe_type_id': m.principio_ativo.lista_medicamento.tipo_receita.co_tipo_receita })} for m in medicamento_list_all]
+    elif table == 'forma_farmaceutica':
+        forma_farmaceutica_list_all = FormaFarmaceutica.query.all()
+        return [{'model': model, 'pk': f.co_forma_farmaceutica, 'fields': json.dumps({'code': f.co_forma_farmaceutica, 'name': f.no_forma_farmaceutica })} for f in forma_farmaceutica_list_all]
+    elif table == 'via_administracao':
+        via_administracao_list_all = ViaAdministracao.query.all()
+        return [{'model': model, 'pk': via.co_aplicacao_medicamento, 'fields': json.dumps({'code': via.co_aplicacao_medicamento, 'name': via.no_aplicacao_med_filtro })} for via in via_administracao_list_all]
+    elif table == 'tipo_receita':
+        tipo_receita_list_all = TipoReceita.query.all()
+        return [{'model': model, 'pk': tp_receita.co_tipo_receita, 'fields': json.dumps({'code': tp_receita.co_tipo_receita, 'name': tp_receita.no_tipo_receita })} for tp_receita in tipo_receita_list_all]
     return []
 
 
