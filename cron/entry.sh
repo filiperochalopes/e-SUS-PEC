daaa7486e8e57d6e00849e1fe0090595cac17810
Filipe Lopes <filiperocklopes@gmail.com>
2024-12-13 09:36:53 +0000

    chore: remove unused files cleaning repo

--- cron/entry.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# start python app
-gunicorn --bind 0.0.0.0:5000 -D --enable-stdio-inheritance --capture-output --log-level debug --reload wsgi:app
-# start cron
-/usr/sbin/crond -f -l 8
